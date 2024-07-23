db78a253f43b7a5175305926a17971018c1b66b0
renaud subrini <renaud.subrini.dev@gmail.com>
2024-07-23 14:42:40 +0200

    tag flex

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -4,9 +4,12 @@ const Tags = ({ tags }) => {
   return (
     <div className="housing-tags">
       {tags.map((tag, tagIndex) => (
+        <div className="tag-container" key={tagIndex}>
         <p key={tagIndex} className="tag">
+
           {tag}
         </p>
+        </div>
       ))}
     </div>
   );
