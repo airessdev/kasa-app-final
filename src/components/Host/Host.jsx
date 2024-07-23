a335498b19128312e7f6c91b66545cdf93c1fcdd
renaud subrini <renaud.subrini.dev@gmail.com>
2024-07-23 21:29:28 +0200

    responsive

--- src/components/Host/Host.jsx
+++ src/components/Host/Host.jsx
@@ -1,44 +1,78 @@
 
 
 
+// import "./Host.css";
+// import Stars from "../Stars/Stars"
+// import Tags from "../Tags/Tags"
+
+// const Host = ({ title, location, tags, rating, name, picture }) => {
+
+//   const [firstName, lastName] = name.split(" ");
+//   //séparer le nom et le prénom de la variable name pour simplifier l'affichage
+//   return (
+
+//     <>
+      
+//         <div className="housing-info">
+
+//           <h1 className="housing-title">{title}</h1>
+//           <p className="housing-location">{location}</p>
+//           <Tags tags={tags} />
+//         </div>
+//         <div>
+//           <div className="housing-host">
+
+//             <div className="host-info">
+
+//               <div className="host-name">
+
+//                 <p>{firstName}</p>
+//                 <p>{lastName}</p>
+//               </div>
+//               <img className="host-img" src={picture} alt="host" />
+//             </div>
+//             <div className="host-rating">
+
+//               {/* Affichage des étoiles de rating utilisation du composant Stars */}
+//               <Stars rating={rating} />
+//             </div>
+//           </div>
+//         </div>
+      
+//     </>
+//   )
+// };
+
+// export default Host;
+
 import "./Host.css";
-import Stars from "../Stars/Stars"
-import Tags from "../Tags/Tags"
+import Stars from "../Stars/Stars";
+import Tags from "../Tags/Tags";
 
 const Host = ({ title, location, tags, rating, name, picture }) => {
-
   const [firstName, lastName] = name.split(" ");
-  //séparer le nom et le prénom de la variable name pour simplifier l'affichage
-  return (
 
+  return (
     <>
       <div className="housing-info">
-
         <h1 className="housing-title">{title}</h1>
         <p className="housing-location">{location}</p>
         <Tags tags={tags} />
       </div>
-
       <div className="housing-host">
-
         <div className="host-info">
-
           <div className="host-name">
-
             <p>{firstName}</p>
             <p>{lastName}</p>
           </div>
           <img className="host-img" src={picture} alt="host" />
         </div>
         <div className="host-rating">
-
-          {/* Affichage des étoiles de rating utilisation du composant Stars */}
           <Stars rating={rating} />
         </div>
       </div>
-
     </>
-  )
+  );
 };
 
 export default Host;
--- src/components/Stars/Stars.jsx
+++ src/components/Stars/Stars.jsx
@@ -1,29 +1,51 @@
-import "./Stars.css"
-import starFullIcon from "../../assets/images/star-full.svg";
-import starEmptyIcon from "../../assets/images/star-empty.svg"
+// import "./Stars.css"
+// import starFullIcon from "../../assets/images/star-full.svg";
+// import starEmptyIcon from "../../assets/images/star-empty.svg"
 
 
 
 
-const Stars = ({rating}) => {
+// const Stars = ({rating}) => {
     
 
-    return (
+//     return (
 
-        <div className="stars">
-            {rating > 0 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
-            {rating > 1 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
-            {rating > 2 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
-            {rating > 3 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
-            {rating > 4 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
-
-
-
-        </div>)
+//         <div className="stars">
+//             {rating > 0 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
+//             {rating > 1 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
+//             {rating > 2 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
+//             {rating > 3 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
+//             {rating > 4 ? <img src={starFullIcon} alt="stars-full" /> : <img src={starEmptyIcon} alt="stars-empty" />}
 
 
 
+//         </div>)
 
-}
 
-export default Stars
+
+
+// }
+
+// export default Stars
+
+import "./Stars.css";
+import starFullIcon from "../../assets/images/star-full.svg";
+import starEmptyIcon from "../../assets/images/star-empty.svg";
+
+const Stars = ({ rating }) => {
+  const totalStars = 5;
+
+  return (
+    <div className="stars">
+      {Array.from({ length: totalStars }, (_, index) => (
+        <img
+          key={index}
+          src={index < rating ? starFullIcon : starEmptyIcon}
+          alt={index < rating ? "star-full" : "star-empty"}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Stars;
